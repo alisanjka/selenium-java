8642182f9a6d3e6a290cae80daffa262616b8da6
alisanjka <alisana@inbox.lv>
2025-04-28 21:28:43 +0300

    [master]: Upload progress

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,3 +1,5 @@
+import lv.acodemy.page_object.InventoryPage;
+import lv.acodemy.page_object.LoginPage;
 import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,6 +12,8 @@ import org.testng.annotations.Test;
 public class SauceDemoTest {
 
     ChromeDriver driver;
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
 
     @BeforeMethod
     public void beforeTest() {
@@ -19,22 +23,31 @@ public class SauceDemoTest {
         // How to call driver methods?
         // URL: www.saucedemo.com
         driver.get("https://www.saucedemo.com");
-        // Need to find element, name input field?
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        // hello -> char sequence h e l l o;
-        //By.id();
-        //By.name();
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        // Enter password!
-        driver.findElement(By.xpath("//input[@data-test='login-button']")).click();
+
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
     }
 
     @Test
     public void verifyLoggedInTest() {
+        loginPage.authorize("standard_user", "secret_sauce");
         String productsText = driver.findElement(By.className("title")).getText();
         Assertions.assertThat(productsText).isEqualTo("Products");
         Assert.assertEquals(productsText, "Products");
         }
+    @Test
+    public void logInTest() {
+        loginPage.authorize("standard_user", "secret_sauce");
+
+    }
+
+    @Test
+    public void addItemToTheCart() {
+        loginPage.authorize("standard_user", "secret_sauce");
+
+
+        inventoryPage.addItemToCartByName("Onesie");
+    }
 
     @AfterMethod()
     public void tearDown() {
